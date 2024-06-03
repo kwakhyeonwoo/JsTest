123c3f00b5356ae093a8fd8b3474f451772820ae
polla <74705730+kwakhyeonwoo@users.noreply.github.com>
2024-06-03 14:32:32 +0900

    Update FavoriteClothes.js
    백틱

--- 웹시개/기말 프로젝트/FavoriteClothes.js
+++ 웹시개/기말 프로젝트/FavoriteClothes.js
@@ -5,17 +5,17 @@ class FavoriteClothes{
 
     addFavorite(clothes){
         this.favorites.push(clothes);
-        console.log("${clothes}이 좋아하는 옷 목록에 추가 되었습니다");
+        console.log(`${clothes}이 좋아하는 옷 목록에 추가 되었습니다`);
     }
 
     removeFavrite(clothes){
         const index = this.favorites.indexOf(clothes);
         if(index !== -1){
             this.favorites.splice(index, 1);
-            console.log("${clothes}이 좋아하는 옷 목록에 제거 되었습니다");
+            console.log(`${clothes}이 좋아하는 옷 목록에 제거 되었습니다`);
         }
         else{
-            console.log("${clothes}이 좋아하는 옷 목록에 없습니다.")
+            console.log(`${clothes}이 좋아하는 옷 목록에 없습니다.`)
         }
     }
 
@@ -23,5 +23,3 @@ class FavoriteClothes{
         return this.favorites;
     }
 }
-
-//전략 패턴 사용해서 상의, 하의, 액세서리 입력 받을 것
