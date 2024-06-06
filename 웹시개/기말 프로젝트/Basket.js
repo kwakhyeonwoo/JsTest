d4ec76dee06f4a30a0a06f1271be06f80084f06f
polla <74705730+kwakhyeonwoo@users.noreply.github.com>
2024-06-06 13:37:53 +0900

    Update Basket.js
    Observer pattern

--- 웹시개/기말 프로젝트/Basket.js
+++ 웹시개/기말 프로젝트/Basket.js
@@ -1,29 +1,48 @@
-class Basket extends ClothesBuy{
-    constructor(){
+class Basket {
+    constructor() {
         this.basket = [];
+        this.observers = [];
     }
-    addToBasket(clothes){
+
+    addObserver(observer) {
+        this.observers.push(observer);
+    }
+
+    removeObserver(observer) {
+        const index = this.observers.indexOf(observer);
+        if (index !== -1) {
+            this.observers.splice(index, 1);
+        }
+    }
+
+    notifyObservers() {
+        this.observers.forEach(observer => observer.update(this.basket));
+    }
+
+    addToBasket(clothes) {
         this.basket.push(clothes);
-        console.log(`${clothes}이 바구니에 추가 되었습니다`);
+        console.log(`${clothes.name}이 바구니에 추가 되었습니다`);
+        this.notifyObservers();
     }
 
-    removeFromBasket(clothes){
+    removeFromBasket(clothes) {
         const index = this.basket.indexOf(clothes);
-        if(index == 1){
+        if (index !== -1) {
             this.basket.splice(index, 1);
-            console.log(`${clothes}이 바구니에서 제거 되었습니다`);
-        }
-        else{
-            console.log(`${clothes}이 바구니에 없습니다`);
+            console.log(`${clothes.name}이 바구니에서 제거 되었습니다`);
+            this.notifyObservers();
+        } else {
+            console.log(`${clothes.name}이 바구니에 없습니다`);
         }
     }
 
-    listBasket(){
+    listBasket() {
         return this.basket;
     }
 
-    clearBasket(){
+    clearBasket() {
         this.basket = [];
         console.log("바구니가 비워졌습니다");
+        this.notifyObservers();
     }
 }
