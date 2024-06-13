e21dca74e0f906e1bb6dd58e68ef43a05ee56cc6
polla <74705730+kwakhyeonwoo@users.noreply.github.com>
2024-06-13 12:08:02 +0900

    Update ClothesSerach.js

--- 웹시개/기말 프로젝트/ClothesSerach.js
+++ 웹시개/기말 프로젝트/ClothesSerach.js
@@ -1,23 +1,48 @@
-class ClothesSearch {
+const Clothes = require('./Clothes.js');
+
+class ClothesSerach {
     constructor(inventory) {
         this.inventory = inventory;
     }
 
     searchByUpper(name) {
-        return this.inventory.filter(clothes => 
-            clothes.type === 'Upper' && clothes.name.toLowerCase().includes(name.toLowerCase())//타입이 Upper인지 확인, 이름에 포함되어 있는지 확인하기 위해 다 소문자로 출력
+        return this.inventory.filter(item =>
+            item.type === 'Upper' && item.name.toLowerCase().includes(name.toLowerCase())
         );
     }
 
     searchByUnder(name) {
-        return this.inventory.filter(clothes => 
-            clothes.type === 'Under' && clothes.name.toLowerCase().includes(name.toLowerCase())
+        return this.inventory.filter(item =>
+            item.type === 'Under' && item.name.toLowerCase().includes(name.toLowerCase())
         );
     }
 
     searchByAccessory(name) {
-        return this.inventory.filter(clothes => 
-            clothes.type === 'Accessory' && clothes.name.toLowerCase().includes(name.toLowerCase())
+        return this.inventory.filter(item =>
+            item.type === 'Accessory' && item.name.toLowerCase().includes(name.toLowerCase())
         );
     }
+
+    search(command, name) {
+        switch (command.toLowerCase()) {
+            case 'upper':
+                return this.searchByUpper(name);
+            case 'under':
+                return this.searchByUnder(name);
+            case 'accessory':
+                return this.searchByAccessory(name);
+            default:
+                return [];
+        }
+    }
+
+    static example() {
+        const inventory = Clothes.getInventory();
+        const clothesSerach = new ClothesSerach(inventory);
+        console.log(clothesSerach.search('upper', 'T-Shirt'));
+        console.log(clothesSerach.search('under', 'Jeans'));
+        console.log(clothesSerach.search('accessory', 'Hat'));
+    }
 }
+
+module.exports = ClothesSerach;
