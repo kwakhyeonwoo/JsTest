ac8301c325f087249c3afd87ab7b8fea36ce50e0
polla <74705730+kwakhyeonwoo@users.noreply.github.com>
2024-06-13 12:09:52 +0900

    Update FavoriteClothes.js

--- 웹시개/기말 프로젝트/FavoriteClothes.js
+++ 웹시개/기말 프로젝트/FavoriteClothes.js
@@ -1,25 +1,25 @@
-class FavoriteClothes{
-    constructor(){
+class FavoriteClothes {
+    constructor() {
         this.favorites = [];
     }
 
-    addFavorite(clothes){
+    addFavorite(clothes) {
         this.favorites.push(clothes);
         console.log(`${clothes}이 좋아하는 옷 목록에 추가 되었습니다`);
     }
 
-    removeFavrite(clothes){
+    removeFavorite(clothes) {
         const index = this.favorites.indexOf(clothes);
-        if(index !== -1){
+        if (index !== -1) {
             this.favorites.splice(index, 1);
-            console.log(`${clothes}이 좋아하는 옷 목록에 제거 되었습니다`);
-        }
-        else{
-            console.log(`${clothes}이 좋아하는 옷 목록에 없습니다.`)
+            console.log(`${clothes}이 좋아하는 옷 목록에서 제거 되었습니다`);
+        } else {
+            console.log(`${clothes}이 좋아하는 옷 목록에 없습니다.`);
         }
     }
 
-    listFavorites(){
+    listFavorites() {
         return this.favorites;
     }
 }
+
