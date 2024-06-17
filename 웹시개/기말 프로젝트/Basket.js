b290185596eb2b9bcc741eefa4d8f08fbd6ef907
polla <74705730+kwakhyeonwoo@users.noreply.github.com>
2024-06-17 14:22:45 +0900

    Update Basket.js
    observer pattern

--- 웹시개/기말 프로젝트/Basket.js
+++ 웹시개/기말 프로젝트/Basket.js
@@ -17,13 +17,14 @@ class Basket {
         }
     }
 
+    // 옵저버 패턴 - 장바구니 상태 변화시 알림을 줌
     notifyObservers() {
         this.observers.forEach(observer => observer.update(this.basket));
     }
 
     addToBasket(clothes) {
         this.basket.push(clothes);
-        console.log(`${clothes.name}이 바구니에 추가 되었습니다`);
+        console.log(`${clothes.name}이/가 바구니에 추가 되었습니다`);
         this.notifyObservers();
     }
 
@@ -31,15 +32,31 @@ class Basket {
         const index = this.basket.indexOf(clothes);
         if (index !== -1) {
             this.basket.splice(index, 1);
-            console.log(`${clothes.name}이 바구니에서 제거 되었습니다`);
+            console.log(`${clothes.name}이/가 바구니에서 제거 되었습니다`);
             this.notifyObservers();
         } else {
-            console.log(`${clothes.name}이 바구니에 없습니다`);
+            console.log(`${clothes.name}이/가 바구니에 없습니다`);
         }
     }
 
     listBasket() {
-        return this.basket;
+        return this.basket.map(item => item.toString());
+    }
+
+    listBasketByType() {
+        const types = {
+            Upper: [],
+            Under: [],
+            Accessory: []
+        };
+
+        this.basket.forEach(item => {
+            if (item.type in types) {
+                types[item.type].push(item.toString());
+            }
+        });
+
+        return types;
     }
 
     clearBasket() {
@@ -56,9 +73,10 @@ class Basket {
     static example() {
         const inventory = Clothes.getInventory();
         const basket = new Basket();
-        basket.addWithDiscount(inventory[0], 0.1); // T-shirt 10% 할인가
-        basket.addToBasket(inventory[2]); // Jeans Basket에 저장
+        basket.addWithDiscount(inventory[0], 0.1); // Apply 10% discount to T-Shirt
+        basket.addToBasket(inventory[2]); // Add Jeans to basket
         console.log(basket.listBasket());
+        console.log(basket.listBasketByType());
     }
 }
 
