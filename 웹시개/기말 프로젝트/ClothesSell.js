3061b4214b98f8dd9eb3ec60235fbe8045900208
polla <74705730+kwakhyeonwoo@users.noreply.github.com>
2024-06-18 11:04:44 +0900

    Update ClothesSell.js

--- 웹시개/기말 프로젝트/ClothesSell.js
+++ 웹시개/기말 프로젝트/ClothesSell.js
@@ -1,15 +1,25 @@
-class ClothesSell extends ClothesBuy{
-    constructor(){
-        this.inventory =[];
+class ClothesSell {
+    constructor(inventory) {
+        this.inventory = inventory;
     }
 
-    sellItem(clothes) {
-        const index = this.inventory.indexOf(item);
-        if (index !== -1) {
-            this.inventory.splice(index, 1);
-            console.log(`${clothes}가 판매되었습니다.`);
-        } else {
-            console.log(`${clothes}가 인벤토리에 없습니다.`);
-        }
+    //Strategy pattern
+    executeSecond(item, strategy) {
+        strategy.execute(item, this.inventory);
     }
 }
+
+// 사용 예제
+const inventory = [
+    { name: 'T-Shirt', type: 'upper', price: 20 },
+    { name: 'Jeans', type: 'under', price: 40 },
+    { name: 'Hat', type: 'accessory', price: 15 }
+];
+
+const clothesSell = new ClothesSell(inventory);
+const removeItemStrategy = new RemoveItemStrategy();
+const logItemStrategy = new LogItemStrategy();
+
+const item = { name: 'Jeans', type: 'under', price: 40 };
+clothesSell.executeSecond(item, removeItemStrategy); // "Jeans 인벤토리에 저장 되었습니다."
+clothesSell.executeSecond(item, logItemStrategy); // "Attempting to remove Jeans from inventory."
